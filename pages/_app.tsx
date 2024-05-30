62321afe7831b6432b6417a0e3aa205cd49cc22b
q-miyata <miyata@q-co.jp>
2024-05-30 21:10:24 +0900

    finished with highlightening bingo line

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,30 +5,28 @@ import { css } from "@emotion/react"
 import { useState } from 'react';
 
 //board から受け取る ({引数/props}:{引数/propsの型定義})
-function Square({ value, onSquareClick, bingoLine }:{ value: string; onSquareClick: () => void; bingoLine: boolean | null} ) {
+function Square({ value, onSquareClick, bingoSquare }:{ value: string; onSquareClick: () => void; bingoSquare: any} ) {
   const squareStyle = [styles.square]
 //配列の後ろの要素がoverrideする。
   if(!value){
     squareStyle.push(styles.emptySquare);
   }
 //true判定だったらビンゴの列に色をつける
-　if(bingoLine){
-  squareStyle.push(styles.winLine);}
-else{
-
+ if(bingoSquare){
+    squareStyle.push(styles.winLine);
+ }
+　//if(winner){}winner とlineを渡さないといけない
   return (
     <button css={squareStyle} onClick={onSquareClick}>
       {value}
     </button>
   );
-}};
+};
 
 
-
-
-function Board({ xIsNext, squares, onPlay }:{xIsNext: boolean; squares: string[]; onPlay: (nextSquares: string[]) => void}) {
+function Board({ xIsNext, squares, onPlay }:{xIsNext: boolean; squares: string[]; onPlay: (nextSquares: string[]) => void; bingoSquare: any}) {
   function handleClick(i: number) {
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares).winner || squares[i]) { //ここでスタックしてた
       return;
     }
     const nextSquares = squares.slice();
@@ -39,11 +37,10 @@ function Board({ xIsNext, squares, onPlay }:{xIsNext: boolean; squares: string[]
     }
     onPlay(nextSquares);
   }
-
   //オブジェクトにアクセス
   //const winner = calculateWinner(squares);
   //caluculatewinner関数でオブジェクトを返しているから
-  const { winner, line } =  calculateWinner(squares);
+  const  {winner, line}=  calculateWinner(squares);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
@@ -51,6 +48,7 @@ function Board({ xIsNext, squares, onPlay }:{xIsNext: boolean; squares: string[]
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
+ console.log(winner, line);
 
   return (
     <>
@@ -59,19 +57,19 @@ function Board({ xIsNext, squares, onPlay }:{xIsNext: boolean; squares: string[]
      {/* squaresという配列をレンダリングしている 　インデックスで管理しているのはBoardコンポーネントだから
      bingoLine, "| null" を入れたら直った何で？*/}
       <div css={styles.boardRow}>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} bingoLine={line && line.includes(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} bingoLine={line && line.includes(0)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} bingoLine={line && line.includes(0)}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} bingoSquare={line?.includes(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} bingoSquare={line?.includes(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} bingoSquare={line?.includes(2)}/>
       </div>
       <div css={styles.boardRow}>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} bingoLine={line && line.includes(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} bingoLine={line && line.includes(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} bingoLine={line && line.includes(5)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} bingoSquare={line?.includes(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} bingoSquare={line?.includes(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} bingoSquare={line?.includes(5)}/>
       </div>
       <div css={styles.boardRow}>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} bingoLine={line && line.includes(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} bingoLine={line && line.includes(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} bingoLine={line && line.includes(8)}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} bingoSquare={line?.includes(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} bingoSquare={line?.includes(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} bingoSquare={line?.includes(8)}/>
       </div>
     </>
   );
@@ -125,7 +123,7 @@ export default function Game() {
 }
 
 //(引数の型)：{返り値の型}
-function calculateWinner(squares: string[]):{ winner: string | null , line: number[] | null } {
+function calculateWinner(squares: string[]) {
   //どのラインが色つくべきなのか情報を取得したい
   const lines = [
     [0, 1, 2],
@@ -139,10 +137,10 @@ function calculateWinner(squares: string[]):{ winner: string | null , line: numb
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-  
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return  { winner: squares[a] , line: lines[i] } //オブジェクトを返す
+      console.log({ winner: squares[a], line: lines[i] });
+      return   { winner: squares[a], line: lines[i] }; //オブジェクトを返す
     }
   }
-  return { winner: null , line: null};
-    }
+  return { winner: null, line: null };
+    };
