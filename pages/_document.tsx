6430b3747c23fa30aa136d3140e8968886522e5d
q-miyata <miyata@q-co.jp>
2024-06-03 10:47:58 +0900

    save this before finalchange

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,16 @@ import { css, Global } from "@emotion/react";
 import { global } from "./_app.styles"
 import Togglebutton from "./darkbutton";
 import { lightTheme, darkTheme } from "./_app.styles";
-import App from "next/app";
+import App from "./_app";
 
 export default function Document() {
- // const theme = darkTheme;
+const theme = darkTheme;
   return (
     <Html lang="en">
       <Head />
-      <body >
-      <App />
+      <body css={theme}>
+        <Global styles={theme} />
+      {/* <App /> */}
        {/* 以下、styles={global}にしてたらダークモードにならなかった */}
      
     
