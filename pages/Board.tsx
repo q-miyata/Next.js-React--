980a5496ab5bd20959e0d90769ad506317c7efa3
q-miyata <miyata@q-co.jp>
2024-06-10 17:20:22 +0900

    save

--- pages/Board.tsx
+++ pages/Board.tsx
@@ -1,5 +1,5 @@
 import { styles } from './_app.styles';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Square from './Square';
 
 type BoardProps = {
@@ -13,7 +13,7 @@ export default function Board({
   squares,
   onPlay,
 }: BoardProps): JSX.Element {
-  function handleClick(i: number) {
+  const handleClick = useCallback((i: number) => {
     if (calculateWinner(squares).winner || squares[i] || isDraw) {
       return;
     }
@@ -25,15 +25,20 @@ export default function Board({
     }
     //This just put arguments to handlePlay function
     onPlay(nextSquares, i);
-  }
+    //dependancyの値が変わらない限り新しい関数インスタンスが生成されない
+  }, []);
 
+  console.log('Board called');
   type WinnerLine = {
     winner: 'X' | 'O' | null;
     line: number[] | null;
     isDraw: boolean;
   };
 
-  const { winner, line, isDraw }: WinnerLine = calculateWinner(squares);
+  const { winner, line, isDraw }: WinnerLine = useMemo(
+    () => calculateWinner(squares),
+    [squares]
+  );
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
@@ -115,16 +120,15 @@ function calculateWinner(squares: Bingo) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  //余力があれば set this false by default
+
   let isDraw = true;
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    console.log([squares[a], squares[b], squares[c]]);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return { winner: squares[a], line: lines[i], isDraw: false };
     }
-    //一つの要素にどっちも含まれているかチェック。これをループ
+    console.log([squares[a], squares[b], squares[c]]);
     const hasX = [squares[a], squares[b], squares[c]].includes('X');
     const hasO = [squares[a], squares[b], squares[c]].includes('O');
 
