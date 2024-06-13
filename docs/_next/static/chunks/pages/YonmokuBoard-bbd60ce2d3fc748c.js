2a541a028cbd397b40a50d3fe3303ecb13addf5f
q-miyata <miyata@q-co.jp>
2024-06-13 16:33:35 +0900

    added git hub pages

---
+++ docs/_next/static/chunks/pages/YonmokuBoard-bbd60ce2d3fc748c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{7732:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/YonmokuBoard",function(){return u(8945)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=7732)}),_N_E=n.O()}]);
