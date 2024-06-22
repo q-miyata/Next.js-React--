66e173f3a0c039a9b6671c3c59e26115fcd0dc42
q-miyata <miyata@q-co.jp>
2024-06-22 15:19:31 +0900

    サーバーサイド完成

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('move', (data) => {
+    socket.broadcast.emit('move', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
